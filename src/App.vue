70889499115c2c9a393f0128f1689a8bedbf5c68
Prateek Mishra <prateekmishra2927@gmail.com>
2022-03-13 22:43:08 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Our compensation management system app"/>
   </div>
 </template>
 
