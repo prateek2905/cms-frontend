d28f307f971b0350528ee31ec830c27893967ee7
Prateek Mishra <prateekmishra2927@gmail.com>
2022-03-22 00:54:01 +0530

    changes on markup and app.vue

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      logo: './assets/logo.png',
+      employee: 'Jane',
+      jobTitle: 'Software Engineer',
+      jobLocation: 'San Francisco',
+      expiry: 'Apr, 1, 2030',
+      teamImage: ''
+    }
   }
 }
 </script>
